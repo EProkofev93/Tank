e30453c01964c523b95876607fbaa8b0365663f5
EProkofev93 <smileeto@gmail.com.com>
2023-07-10 16:53:34 +0300

    finish

--- src/Tank.java
+++ src/Tank.java
@@ -1,42 +1,129 @@
 public class Tank {
-    int x = 0;
-    int y = 0;
+    private int x = 0;
+    private int y = 0;
 
-    int fuel = 100;
-    char orient = 's';
+    private int fuel = 100;
+    private char orient = 's';
+
+    final String model = "T34";
+
+    private static int tankCount = 1;
+
+    private int id = tankCount;
 
 
     void goForward(int i) {
+        i = burn(i);
 
-        if (orient == 's') {
-            x += i;
+        go(i);
+
+    }
+
+
+    public int burn(int a) {
+        if (a > 0) {
+            if (fuel <= a) {
+                a = fuel;
+                fuel -= a;
+                System.out.println("fuel is out");
+            }
+            if (fuel > a) {
+                fuel -= a;
+            }
+
+            return a;
+        } else {
+            a = -a;
+            if (fuel <= a) {
+                a = fuel;
+                fuel -= a;
+                System.out.println("fuel is out");
+            }
+            if (fuel > a) {
+                fuel -= a;
+            }
+            a = -a;
+
+            return a;
+
         }
-        if (orient == 'b') {
-            x -= i;
-        }
-        if (orient == 'r') {
-            y += i;
-        }
-        if (orient == 'l') {
-            y -= i;
-        }
-        burn(i);
 
 
     }
 
+
+    void go(int fuel2) {
+
+
+        if (orient == 's') {
+            x += fuel2;
+        }
+        if (orient == 'b') {
+            x -= fuel2;
+        }
+        if (orient == 'r') {
+            y += fuel2;
+        }
+        if (orient == 'l') {
+            y -= fuel2;
+        }
+
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public char getOrient() {
+        return orient;
+    }
+
+    public void setOrient(char orient) {
+        this.orient = orient;
+    }
+
+    public static int getTankCount() {
+        return tankCount;
+
+    }
+
+
     void goBackward(int i) {
 
-        goForward(-i);
-        burn(i);
-        burn(i);
+        i = burn(i);
+
+        go(-i);
+
 
     }
 
     void printPosition() {
 
-        System.out.println("The Tank is at " + x + ", " + y + " now.");
+        System.out.println("The " + this.model + "-" + id + " is at " + x + ", " + y + " now.");
     }
+
 
     void turnRight() {
 
@@ -80,13 +167,25 @@ public class Tank {
         }
     }
 
-    void burn(int i) {
 
-        fuel -= i;
+    public Tank() {
+        this(0, 0, 100);
+    }
 
-        if (fuel <= 0) {
-            System.out.println("Fuel is out");
-        }
+    public Tank(int x, int y) {
+        this(x, y, 100);
+
+    }
+
+
+    public Tank(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+        tankCount++;
+
 
     }
 }
+
+
